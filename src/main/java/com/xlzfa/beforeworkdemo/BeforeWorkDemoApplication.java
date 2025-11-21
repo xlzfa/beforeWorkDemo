02237402758be36ac3f3721dcb9d32191444903f
xlzfa <www55555lala@163.com>
2025-11-21 20:27:00 +0800

    项目初始创建

---
+++ src/main/java/com/xlzfa/beforeworkdemo/BeforeWorkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xlzfa.beforeworkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeforeWorkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeforeWorkDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xlzfa/beforeworkdemo/BeforeWorkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xlzfa.beforeworkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeforeWorkDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
