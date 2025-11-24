a890bbece6532252d7a33ebb5902fd1366ab4f4d
xlzfa <www55555lala@163.com>
2025-11-24 15:52:58 +0800

    统一返回对象

---
+++ src/main/java/com/xlzfa/beforeworkdemo/common/ResponseResult.java
@@ -0,0 +1,35 @@
+package com.xlzfa.beforeworkdemo.common;
+
+public class ResponseResult {
+
+    private Integer code;
+    private String msg;
+    private Object data;
+
+    //无参构造
+    public ResponseResult() {
+    }
+
+    //有参构造
+    public ResponseResult(Integer code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    //成功，无数据可返回
+    public static ResponseResult sucess(){
+        return new ResponseResult(200, "success", null);
+    }
+
+    //成功，返回数据
+    public static ResponseResult success(Object data){
+        return new ResponseResult(200, "success", data);
+    }
+
+    //失败,返回失败信息
+    public static ResponseResult error(String msg){
+        return new ResponseResult(500, msg, null);
+    }
+
+}
