82f3efd511a0a96e518572fc9ac2f5b8625fcc32
xlzfa <www55555lala@163.com>
2025-11-24 16:26:07 +0800

    全局异常处理

--- src/main/java/com/xlzfa/beforeworkdemo/common/GlobalExceptionHandler.java
+++ src/main/java/com/xlzfa/beforeworkdemo/common/GlobalExceptionHandler.java
@@ -1,7 +1,44 @@
 package com.xlzfa.beforeworkdemo.common;
 
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理
+ */
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * 自定义业务异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(BusinessException.class)
+    public ResponseResult handleBusinessException(BusinessException ex) {
+        return ResponseResult.error(ex.getCode(), ex.getMessage());
+    }
+
+    /**
+     * 运行时异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseResult handleRuntimeException(RuntimeException ex) {
+        return ResponseResult.error(500, "系统运行异常： " + ex.getMessage());
+    }
+
+    /**
+     * 其他异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseResult handleException(Exception ex) {
+        return ResponseResult.error(500, "未知异常： " + ex.getMessage());
+    }
 
 
 }
