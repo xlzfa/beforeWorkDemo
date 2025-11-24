39b32a3f853f538db2d28221daef8311d72dac83
xlzfa <www55555lala@163.com>
2025-11-24 16:06:46 +0800

    优化代码规范

--- src/main/java/com/xlzfa/beforeworkdemo/common/ResponseResult.java
+++ src/main/java/com/xlzfa/beforeworkdemo/common/ResponseResult.java
@@ -18,17 +18,17 @@ public class ResponseResult {
     }
 
     //成功，无数据可返回
-    public static ResponseResult sucess(){
+    public static ResponseResult sucess() {
         return new ResponseResult(200, "success", null);
     }
 
     //成功，返回数据
-    public static ResponseResult success(Object data){
+    public static ResponseResult success(Object data) {
         return new ResponseResult(200, "success", data);
     }
 
     //失败,返回失败信息
-    public static ResponseResult error(String msg){
+    public static ResponseResult error(String msg) {
         return new ResponseResult(500, msg, null);
     }
 
